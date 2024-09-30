335eb7cd81af6fa2122c5be3979c124e50464d97
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-10-01 02:55:40 +0500

    Task1

---
+++ Tyuiu.KurbanovFA.Sprint2.Task1.V15.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.KurbanovFA.Sprint2.Task1.V15.Lib
+{
+    public class DataService : ISprint2Task1V15
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            /*int a = 415; tffft
+            int b = 616;
+            int c = 134;
+            int d = 127;*/
+
+            bool[] valueArray = new bool[6];
+            valueArray[0] = a == d | a > c;
+            valueArray[1] = a != d & a < c;
+            valueArray[2] = a > b || a < c;
+            valueArray[3] = a >= b && a == c;
+            valueArray[4] = a == d !;
+            valueArray[5] = a > b ^ a != c;
+
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.KurbanovFA.Sprint2.Task1.V15/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.KurbanovFA.Sprint2.Task1.V15.Lib;
+
+namespace Tyuiu.KurbanovFA.Sprint2.Task1.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int a = 415;
+            int b = 616;
+            int c = 134;
+            int d = 127;
+
+            Console.Title = "Спринт #2 | Выполнил: Курбанов Ф.А. | РППб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Курбанов Фархаджон Азамжанович | РППб-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность можно чередовать, но использовать один раз в         *");
+            Console.WriteLine("* выражении) и логических операций (|, &, ||, &&, !, ^,                   *");
+            Console.WriteLine("* последовательность операций не должна нарушаться), а также              *");
+            Console.WriteLine("* арифметических выражений, которая вернет логическую                     *");
+            Console.WriteLine("* последовательность(массив): (True, False, False, False, False, True),   *");
+            Console.WriteLine("* при a = 415, b = 616, c = 134, d = 127                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* A = " + a);
+            Console.WriteLine("* B = " + b);
+            Console.WriteLine("* C = " + c);
+            Console.WriteLine("* D = " + d);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c ,d);
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
